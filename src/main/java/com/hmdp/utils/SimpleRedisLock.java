024736b0dca0ae65dd60895d72dc28621da89799
xuhhhhux <1126964021@qq.com>
2024-03-16 13:44:03 +0800

    feat: lua脚本实现释放分布式锁原子性

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,16 +1,27 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
 
     private final String name;
     private final StringRedisTemplate stringRedisTemplate;
-    private final String KEY_PREFIX = "lock:";
+    private static final String KEY_PREFIX = "lock:";
     private final String VALUE_PREFIX = UUID.randomUUID().toString(true);
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
@@ -30,9 +41,16 @@ public class SimpleRedisLock implements ILock{
     public void unLock() {
         String key = KEY_PREFIX + name;
         String threadId = VALUE_PREFIX + "-" + Thread.currentThread().getId();
-        String id = stringRedisTemplate.opsForValue().get(key);
-        if (id != null && id.equals(threadId)) {
-            stringRedisTemplate.delete(key);
-        }
+        stringRedisTemplate.execute(UNLOCK_SCRIPT, Collections.singletonList(key), threadId);
     }
+
+//    @Override
+//    public void unLock() {
+//        String key = KEY_PREFIX + name;
+//        String threadId = VALUE_PREFIX + "-" + Thread.currentThread().getId();
+//        String id = stringRedisTemplate.opsForValue().get(key);
+//        if (id != null && id.equals(threadId)) {
+//            stringRedisTemplate.delete(key);
+//        }
+//    }
 }
